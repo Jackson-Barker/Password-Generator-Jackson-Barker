5793c4d9d7084309b7904c7150cbad467dce5d71
jbarker <jackson.barker@du.edu>
2021-09-11 21:22:01 -0600

    variable assignments

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
-// Assignment Code
+// Variable Assignments
+//
 var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+var storedPassword = [];
+var numberLength = 0;
+var finalPassword = "";
