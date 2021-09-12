672770b2092a1fa6deef0d08d31d8933f7e49009
jbarker <jackson.barker@du.edu>
2021-09-12 16:23:05 -0600

    function progress and event listener

--- script.js
+++ script.js
@@ -24,13 +24,37 @@ upper = [],
  
  upperLetters = letters.map(toUpper);
 
+ var generateBtn = document.querySelector("#generate");
+
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", function() {
+  finalPassword = writePassword();
+  document.getElementById("password").placeholder = finalPassword;
+});
 
 // function
 function writePassword() {
+  
+  pwlength = parseInt(prompt("How many characters do you want in your password? (8-128)"));
 
-number = confirm("Do you want numbers?")
-upperCase = confirm("Do you want upper case letters?")
-lowerCase = confirm("Do ypu want lower case letters?")
-characters = confirm("Do you want special characters?") 
+  if (!pwlength) {
+    alert("This field is required");
+  } else if (pwlength < 8 || pwlength > 128) {
+    pwlength = parseInt(promt("The number must be between (8-124"))
+  
+  } else { 
+
+  number = confirm("Do you want numbers?");
+  upperCase = confirm("Do you want upper case letters?");
+  lowerCase = confirm("Do ypu want lower case letters?");
+  characters = confirm("Do you want special characters?");
+  
+};
+
+var password = [];
+
+var finalPassword = password.join("");
+UserPrefrence(finalPassword);
+return (finalPassword);  
+}
+
