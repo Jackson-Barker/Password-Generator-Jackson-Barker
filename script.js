21572ad3a5144a2d77058856d16c255d35cf8c11
jbarker <jackson.barker@du.edu>
2021-09-12 14:15:52 -0600

    password variables

--- script.js
+++ script.js
@@ -4,4 +4,20 @@ var pwlength;
 var number;
 var upperCase;
 var lowerCase;
-var Character;
+var character;
+
+
+
+// password variables
+
+number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+letters = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"]
+character = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "{", "}", "<", ">", "?", "/", "|", "[", "]"]
+upper = [],
+
+
+// function
+function writePassword() {
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
