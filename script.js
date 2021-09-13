2aaf7ce36bdd06a9119134bee63b010667ec4875
jbarker <jackson.barker@du.edu>
2021-09-12 19:47:28 -0600

    else if and for loops

--- script.js
+++ script.js
@@ -40,7 +40,7 @@ function writePassword() {
   if (!pwlength) {
     alert("This field is required");
   } else if (pwlength < 8 || pwlength > 128) {
-    pwlength = parseInt(promt("The number must be between (8-124"))
+    pwlength = parseInt(prompt("The number must be between (8-124"));
   
   } else { 
 
@@ -51,7 +51,98 @@ function writePassword() {
   
 };
 
+if (!characters && !number && !upperCase && !lowerCase) {
+  choices = alert("You are required to chose at least one!");
+
+}
+
+else if (characters && number && upperCase && lowerCase) {
+
+  choices = characters.concat(number, upperCase, lowerCase);
+
+}
+
+// 3
+
+else if (characters && number && upperCase) {
+  choices = characters.concat(number, upperCase);
+}
+
+else if (characters && number && lowerCase) {
+  choices = characters.concat(number, lowerCase); 
+}
+
+else if (characters && lowerCase && upperCase) {
+  choices = characters.concat(lowerCase, upperCase);
+
+}
+
+else if (number && lowerCase && upperCase) {
+  choices = number.concat(lowerCase, upperCase);
+
+}
+
+// 2
+
+else if (characters && number) {
+  choices = characters.concat(number);
+
+}
+
+else if (characters && lowerCase) {
+  choices = characters.concat(lowerCase);
+}
+
+else if (characters && upperCase) {
+  choices = characters.concat(upperCase);
+}
+
+else if (lowerCase && number) {
+  choices = lowerCase.concat(number);
+}
+
+else if ( lowerCase && upperCase) {
+  choices = lowerCase.concat(upperCase);
+
+}
+
+else if (number && upperCase) {
+  choices = number.concat(upperCase);
+
+}
+
+// 1
+
+else if (characters) {
+  choices = characters;
+
+}
+
+else if (number) {
+  choices = number;
+
+}
+
+else if (lowerCase) {
+  choices = lowerCase;
+}
+
+// upp
+
+else if (upperCase) {
+  choices = upper.concat(upperCase)
+
+};
+
 var password = [];
+
+for (var i = 0; i < pwlength; i++) {
+  var pickpwLength = choices[Math.floor(Math.random() * choices.length)];
+
+}
+
+
+
 
 var finalPassword = password.join("");
 UserPrefrence(finalPassword);
